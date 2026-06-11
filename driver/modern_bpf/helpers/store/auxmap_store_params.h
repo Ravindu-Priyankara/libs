55ad181b7a9aafc507f999c28323bf67eac7a3b1
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2026-06-11 12:08:23 +0530

    refact: change memcpy to pointer assigning

--- driver/modern_bpf/helpers/store/auxmap_store_params.h
+++ driver/modern_bpf/helpers/store/auxmap_store_params.h
@@ -95,10 +95,12 @@ static __always_inline void auxmap__preload_event_header(struct auxiliary_map *a
 
 	uint8_t nparams = maps__get_event_num_params(event_type);
 	uint64_t ts = maps__get_boot_time() + bpf_ktime_get_boot_ns();
-	uint64_t tid = (uint32_t)bpf_get_current_pid_tgid();
+	uint32_t tid = (uint32_t)bpf_get_current_pid_tgid();
+
+	*(uint64_t *)&auxmap->data[offsetof(struct ppm_evt_hdr, tid)] = (uint64_t)tid;
 
 	__builtin_memcpy(&auxmap->data[offsetof(struct ppm_evt_hdr, ts)], &ts, sizeof(ts));
-	__builtin_memcpy(&auxmap->data[offsetof(struct ppm_evt_hdr, tid)], &tid, sizeof(tid));
+	//__builtin_memcpy(&auxmap->data[offsetof(struct ppm_evt_hdr, tid)], &tid, sizeof(tid));
 	__builtin_memcpy(&auxmap->data[offsetof(struct ppm_evt_hdr, type)], &event_type, sizeof(event_type));
 	__builtin_memcpy(&auxmap->data[offsetof(struct ppm_evt_hdr, nparams)], &nparams, sizeof(nparams));
 
